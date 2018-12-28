00b1832d6c036cac45451eec4e87d4e36963329a
brennowilliam <brennowilliam@gmail.com>
2018-12-28 20:35:13 -0200

    adds modified profile pic and add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,7 +52,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-131427232-1`,
       },
     },
     `gatsby-plugin-feed`,
